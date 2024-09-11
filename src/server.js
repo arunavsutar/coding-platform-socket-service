a015b7ee4425b24e0d71c4929d8bf49bbc639e5e
Arunav <arunavsutar@gmail.com>
2024-09-11 22:06:03 +0530

    Added code for result updation event through the socket.

--- src/server.js
+++ src/server.js
@@ -2,22 +2,36 @@
 const express = require('express');
 const { createServer, get } = require("http");
 const { Server } = require("socket.io");
+const bodyParser = require('body-parser');
 const Redis = require('ioredis');
 
-const redisCache = new Redis();
+const redisCache = new Redis({
+    host: "127.0.0.1",
+    port: 6379,
+    maxRetriesPerRequest: null
+});
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:5500",
+        origin: "*",//http://localhost:5500
         methods: ["GET", "POST"]
     }
 });
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
     console.log("Connected to the CLient");
-    socket.on('setUserId', (userId) => {
+    socket.on('setUserId', async (userId) => {
+
+        await redisCache.set(userId, socket.id);
         console.log(`Setting ${userId} to the connection ID - ${socket.id}`);
-        redisCache.set(userId, socket.id);
+        const exists = await redisCache.exists(userId);
+        console.log(`Key exists: ${exists}`);
+        const val = await redisCache.get(userId);
+        console.log(val);
+
     });
     socket.on('getConnectionId', async (userId) => {
 
@@ -27,20 +41,24 @@ io.on("connection", (socket) => {
     });
 });
 app.post('/sendPayLoad', async (req, res) => {
-    console.log(req.body);
+    console.log("\nThe request came after evaluation :-\n", req.body);
     const { userId, payload } = req.body;
     if (!userId || !payload) {
         return res.status(400).send("Invalid Request.");
     }
-    const socketId = await redisCache.get(userId);
-    if (socketId) {
-        io.to(socketId).emit('submissionPayloadResponse', payload);
-        return res.send('Payload Sent Successfully');
-    }
-    else {
-
-        return res.status(404).send("User not connected");
-    }
+    const ttl = await redisCache.ttl(Number(userId));
+    console.log(`TTL: ${ttl}`);
+    redisCache.get(Number(userId), (err, socketId) => {
+        console.log("The Socket ID - ", socketId); // Prints "value"
+        if (socketId) {
+            io.to(socketId).emit('submissionPayloadResponse', payload);
+            return res.send('Payload Sent Successfully');
+        }
+        else if (err) {
+            console.log(err);
+            return res.status(404).send("User not connected");
+        }
+    });
 });
 httpServer.listen(5000, () => {
     console.log(`Server is Running on port ${5000} `);
